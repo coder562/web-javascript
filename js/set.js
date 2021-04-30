1841a09661c484ad946f59213890e920ae8f75b1
coder562 <vaishalijain654@gmail.com>
2021-04-30 17:45:57 +0530

    new

---
+++ js/set.js
@@ -0,0 +1,4 @@
+document.getElementById("a").innerHTML="<center><h1>hii hhh</h1></center>"
+document.getElementById("b").innerHTML="<h1>Phone:676867677</h1>"
+document.getElementById("a").setAttribute("style","border:10px dotted red");
+document.getElementById("a").removeAttribute("style");
